84b40c878b115f340706fdcae05c23ad9286371f
GuiLouSilva <guilhermesilva@taller.net.br>
2024-11-06 00:57:39 -0300

    feat: finish email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,9 +2,23 @@ require "rqrcode"
 require "mini_magick"
 require "stringio"
 
+html = ApplicationController.render(
+  template: "tickets/image_template", # Caminho do template
+  layout: false,
+  assigns: {
+    order_item: @order_item,
+    customer_email: @customer_email,
+    customer_name: @customer_name,
+    ticket_code: @ticket_code,
+    ticket: @ticket
+  }
+)
+
+
 class OrderMailer < ApplicationMailer
   def order_confirmation(order_item)
     @order_item = order_item
+    @order_id = order_item.id
     @customer_email = order_item.customer_email
     @customer_name = order_item.customer_name
     @ticket_code = order_item.ticket_code
@@ -12,11 +26,35 @@ class OrderMailer < ApplicationMailer
 
     if @ticket_code.present?
       begin
-        qr_code_image = generate_qr_code_image(@ticket_code.uuid)
-        attachments.inline["qrcode.png"] = {
-          mime_type: "image/png",
-          content: qr_code_image.to_blob
-        }
+        @qr_code_image = generate_qr_code_image(@ticket_code.uuid)
+        @qr_code_data_url = "data:image/png;base64,#{Base64.strict_encode64(@qr_code_image.to_blob)}"
+
+        banner_path = Rails.root.join("app", "assets", "images", "banner.jpg")
+        @banner_data_url = "data:image/jpeg;base64,#{Base64.strict_encode64(File.read(banner_path))}"
+
+        html = render_to_string(
+          template: "tickets/ticket_template",
+          layout: false,
+          locals: { qr_code_data_url: @qr_code_data_url }
+        )
+
+        tmp_html_path = Rails.root.join("tmp", "ticket_template.html")
+        File.open(tmp_html_path, "w") { |file| file.write(html) }
+
+        output_path = Rails.root.join("public", "ticket_image_#{@order_id}.png")
+        wkhtmltoimage_options = [
+          "--width 500",
+          "--height 600",
+          "--quality 40",
+          "--enable-local-file-access"
+        ].join(" ")
+
+        `wkhtmltoimage #{wkhtmltoimage_options} #{tmp_html_path} #{output_path}`
+
+        attachments["ingresso.png"] = File.read(output_path)
+
+        File.delete(tmp_html_path) if File.exist?(tmp_html_path)
+        File.delete(output_path) if File.exist?(output_path)
       rescue => e
         puts e.backtrace
       end
@@ -24,7 +62,7 @@ class OrderMailer < ApplicationMailer
 
     mail(
       to: @order_item.customer_email,
-      subject: "Confirmação do seu pedido com QR Code"
+      subject: "Confirmação do seu pedido!"
     )
   end
 
@@ -37,9 +75,9 @@ class OrderMailer < ApplicationMailer
       border_modules: 4,
       color_mode: ChunkyPNG::COLOR_GRAYSCALE,
       color: "black",
-      fill: "white",
+      fill: "#f4f4f4",
       module_px_size: 6,
-      size: 420
+      size: 520
     )
 
     image = MiniMagick::Image.read(StringIO.new(png.to_s))
